0a4ab1c7399673aa35821992bc6e8ae1ab085e85
CarterBelnap <carterbelnap@icloud.com>
2023-02-08 11:05:16 -0500

    Started Grid

---
+++ main.py
@@ -0,0 +1,11 @@
+# Carter Belnap
+# Feburary 7th 2023
+# Battleship Program
+spot =["*"]*25
+run=True
+while run:
+
+    print(" -----")
+
+    print( {spot(i)} {spot(i+1)} {spot(i+2)} {spot(i+3)} {spot(i+4)} )
+    i+=5

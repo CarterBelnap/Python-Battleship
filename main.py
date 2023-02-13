b38dfc6569f9f4d168647bed769f6821e42b26e1
CarterBelnap <carterbelnap@icloud.com>
2023-02-13 11:05:10 -0500

    Finsihed Board

--- main.py
+++ main.py
@@ -4,17 +4,18 @@
 
 import random,os
 
-spot =["*"]*25
+spot =["~"]*25
 
 run=True
 #Game Start
 
 while run:
-    
+    ammo = 10
 #Printing Board
     i=0
     row=1
-
+    print("\n""Welcome To Battelship! Try To Sink My Ships!")
+    print("\n" )
     print("  A B C D E")
     print("-----------")
     while i<25:
@@ -22,32 +23,43 @@ while run:
         i+=5
         row+=1
 
-        guess=(input("Guess Where The Ships Are!:"))
-        letter=guess[0]
-        row=(guess[1]-1)*5
-        
-        if letter=="A":
-            start_v=0
-        elif letter=="B":
-            start_v=1
-        elif letter=="C":
-            start_v=2
-        elif letter=="D":
-            start_v=4
-        elif letter=="E":
-            start_v=5
-        else letter=-1:
-            print("Please Print A Valid Number")
-
-
+    guess=(input("Guess Where The Ships Are!:"))
+    letter=guess[0]
+    row=(int(guess[1])-1)*5
     
-        if spot[row + start_v]=="M":
-            print("You Have Already Guessed Here")
-        else:
-            spot[guess]= "M"
+    if letter=="A":
+        start_v=0
+    elif letter=="B":
+        start_v=1
+    elif letter=="C":
+        start_v=2
+    elif letter=="D":
+        start_v=3
+    elif letter=="E":
+        start_v=4
+    else:
+        print("Please Print A Valid Letter")
+
+    if spot[row + start_v]=="M":
+        print("You Have Already Guessed Here")
+    else:
+        spot[row + start_v]= "M"
+        ammo=-1
+    
+    if row>=6:
+        print("Please Print A Valid Number")
+   
         
     os.system("cls")        
 
         
-    random_num = random.randint(0,25) 
+shiprandom = random.randint(0,11) 
+  
+  
+  #BOARD
+  # 1 2 2 4 4 
+  # 1 3 3 3 5
+  # 7 7 6 6 5
+  # 8 9  9  9  10
+  # 8 11 11 11 10
   

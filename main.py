a571e1a42a0a652b672fc587cc09edb34e0d84b2
CarterBelnap <carterbelnap@icloud.com>
2023-02-14 11:05:01 -0500

    Started RNG

--- main.py
+++ main.py
@@ -2,27 +2,38 @@
 # Feburary 7th 2023
 # Battleship Program
 
-import random,os
-
-spot =["~"]*25
-
-run=True
+#Imports
+import random,os,time
 #Game Start
+run=True
+e=1
+board=[]
+#Board Printing
+while e<=25:
+    board.append("A"+str(e))
+    board.append("B"+str(e))
+    board.append("C"+str(e))
+    board.append("D"+str(e))
+    board.append("E"+str(e))
+    e+=1
 
 while run:
-    ammo = 10
-#Printing Board
+   
+#Board/UI Printing
     i=0
-    row=1
+    row=1 
+    ammo = 10
     print("\n""Welcome To Battelship! Try To Sink My Ships!")
     print("\n" )
-    print("  A B C D E")
-    print("-----------")
+    print("  A  B  C  D  E ")
+    print("----------------")
     while i<25:
-        print(f"{row} {spot[i]} {spot[i+1]} {spot[i+2]} {spot[i+3]} {spot[i+4]}")
+        print(f"{row} {board[i]} {board[i+1]} {board[i+2]} {board[i+3]} {board[i+4]}")
         i+=5
         row+=1
 
+#ASCII Guessing UI
+    print(f"Ammo = {ammo}")
     guess=(input("Guess Where The Ships Are!:"))
     letter=guess[0]
     row=(int(guess[1])-1)*5
@@ -39,24 +50,23 @@ while run:
         start_v=4
     else:
         print("Please Print A Valid Letter")
-
-    if spot[row + start_v]=="M":
-        print("You Have Already Guessed Here")
+        start_v=-1
+    if not start_v==-1 and not int(guess[1])>5:
+        if board[row + start_v]=="M ":
+            print("You Have Already Guessed Here")
+        else:
+            board[row + start_v]= "M "
+            
     else:
-        spot[row + start_v]= "M"
-        ammo=-1
-    
-    if row>=6:
-        print("Please Print A Valid Number")
-   
+        print("Please Enter A Valid Number!")
+        time.sleep(2)
         
     os.system("cls")        
 
         
 shiprandom = random.randint(0,11) 
   
-  
-  #BOARD
+  #BOARD CARD
   # 1 2 2 4 4 
   # 1 3 3 3 5
   # 7 7 6 6 5
